9d279a5476eab8e9bf10fdb4e6a5e0b6729d1a7a
Alfonso Gallardo Rodríguez <alfonso@MacBook-Pro-de-Alfonso.local>
2022-02-11 13:39:46 +0100

    Creando proyecto

---
+++ MiarmaApi/src/main/java/com/salesianostriana/dam/MiarmaApi/MiarmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.MiarmaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiarmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiarmaApiApplication.class, args);
+	}
+
+}
---
+++ MiarmaApi/src/test/java/com/salesianostriana/dam/MiarmaApi/MiarmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.MiarmaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiarmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
